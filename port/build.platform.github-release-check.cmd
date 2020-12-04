020a0f0f506120589e8e41ec7140d8bc04e52258
Grigore Stefan <g_stefan@yahoo.com>
2020-12-04 13:57:08 +0200

    update

--- port/build.platform.github-release-check.cmd
+++ port/build.platform.github-release-check.cmd
@@ -5,7 +5,7 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-echo -^> git-release-check quantum-script-extension-sshremote
+echo -^> github-release-check quantum-script-extension-sshremote
 
 set PROJECT=quantum-script-extension-sshremote
 if not exist source\%PROJECT%.version.ini echo Error - no version & exit 1
