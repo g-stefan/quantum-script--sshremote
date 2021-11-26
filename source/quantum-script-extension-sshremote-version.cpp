f78ec73a6a8c2491fb31079ac41b011480dd3572
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:24:29 +0200

    Update

--- source/quantum-script-extension-sshremote-version.cpp
+++ source/quantum-script-extension-sshremote-version.cpp
@@ -15,10 +15,10 @@ namespace Quantum {
 			namespace SSHRemote {
 				namespace Version {
 
-					static const char *version_ = "2.2.0";
+					static const char *version_ = "2.4.0";
 					static const char *build_ = "20";
-					static const char *versionWithBuild_ = "2.2.0.20";
-					static const char *datetime_ = "2021-11-15 16:51:54";
+					static const char *versionWithBuild_ = "2.4.0.20";
+					static const char *datetime_ = "2021-11-22 17:08:05";
 
 					const char *version() {
 						return version_;
